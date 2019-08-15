43eea4207d5a513d7e33872c357ab30d33f5bfc9
Liujinghhh <1944465209@qq.com>
2019-08-15 16:57:23 +0800

    feat : home page (it nedd be modified

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+ <div class="home">
+   <img class="background" src="../assets/images/home.png">
+   <p class="button">开始</p>
+     
+ </div>
+
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="less" scoped>
+.background{
+    width: 750px;
+    height: 1334px;
+}
+.button{
+   color: #ffffff ;
+   position: fixed; 
+   top: 1100px;
+   left: 610px;   
+   writing-mode: vertical-lr;
+   font-size:40px
+}
+
+
+</style>
