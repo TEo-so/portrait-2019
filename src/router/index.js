159d5a47fa9f615a533cb9687e2f528898700ce8
Liujinghhh <1944465209@qq.com>
2019-08-19 11:11:39 +0800

    docs : 路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,37 +1,39 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import Home from '@/views/Home.vue'
-import Question from '@/views/Question.vue'
-import Loading from '@/views/Loading.vue'
-import Result from '@/views/result.vue'
+
+const Home = () => import('@/views/Home.vue')
+const Question = () => import('@/views/Question.vue')
+const Loading = () => import('@/views/Loading.vue')
+const Result = () => import('@/views/result.vue')
+
 
 Vue.use(Router)
 
 export default new Router({
     routes: [
         {
-            path:'/',
-            name:'home',
-            component:Home,
+            path: '/',
+            name: 'home',
+            component: Home,
         },
         {
-            path:'/question',
-            name:'question',
-            component:Question
+            path: '/question',
+            name: 'question',
+            component: Question
         },
         {
-            path:'/loading',
-            name:'loading',
-            component:Loading
+            path: '/loading',
+            name: 'loading',
+            component: Loading
         },
         {
-            path:'/result',
-            name:'resulr',
-            component:Result
+            path: '/result',
+            name: 'resulr',
+            component: Result
         },
 
-            
+
     ]
 
 })
